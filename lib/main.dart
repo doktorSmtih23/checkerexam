b431b839d815329aef45cf054ecad573ee51e663
doktorSmtih23 <csordonez@hotmail.com>
2024-10-16 19:41:23 -0500

    cambios en main

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Corregidor Simulacros',
+      title: 'Corrector Simulacros',
       theme: ThemeData(
         brightness: Brightness.dark,
         primarySwatch: Colors.blue,
